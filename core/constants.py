4b54e91a8ef4d5e645bdb3d3bcf20e61ffd10578
LEE-HYUNSANG <131837885+LEE-HYUNSANG@users.noreply.github.com>
2025-06-11 22:59:30 +0900

    fix default 1h volume

--- core/constants.py
+++ core/constants.py
@@ -3,7 +3,7 @@ DEFAULT_COIN_SELECTION = {
     "min_price": 700,
     "max_price": 70000,
     "min_volume_24h": 1400000000,
-    "min_volume_1h": 35000000,
+    "min_volume_1h": 10000000,
     "min_tick_ratio": 0.035,
     "excluded_coins": ["KRW-ETHW", "KRW-ETHF", "KRW-XCORE", "KRW-GAS", "KRW-BTS"],
 }
